0275673618010f096a60dd1e77048855d71abea3
Emma Bindi <emma.bindi@outlook.com>
2020-08-20 10:12:04 +1000

    Form input and creating a file with dummy text

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 
 // function rqListener(req, res) {}
 
@@ -7,7 +8,29 @@ const http = require("http");
 
 // ES6 version can also use function rather than above
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text name="message"><button type="submit">Send</button></input></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from NodeJS!</body></h1>");
+  res.write("</html>");
+  res.end();
 });
 
 // tell the server to listen for requests on Port 3000
