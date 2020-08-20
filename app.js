37d68f6a0b31e5913e1d64180392994e097fc8ab
Emma Bindi <emma.bindi@outlook.com>
2020-08-20 11:04:24 +1000

    Place routes and http requests into separate module

--- app.js
+++ app.js
@@ -1,48 +1,7 @@
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
 
-// function rqListener(req, res) {}
+// hey server, execute the function that is inside routes on every incoming request
+const server = http.createServer(routes);
 
-// // this function will run everytime it is requested
-// http.createServer(rqListener);
-
-// ES6 version can also use function rather than above
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title><head>");
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      // console.log(parsedBody);
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title><head>");
-  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-  res.write("</html>");
-  res.end();
-});
-
-// tell the server to listen for requests on Port 3000
 server.listen(3000);
