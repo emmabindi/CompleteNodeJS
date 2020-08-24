1a7e12163c711160c4fd235e0e78e7bb38cf6aaf
Emma Bindi <emma.bindi@outlook.com>
2020-08-24 11:51:00 +1000

    Adding error methods to product model and setup Fetch all

--- controllers/products.js
+++ controllers/products.js
@@ -11,20 +11,20 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  console.log(req.body);
   const product = new Product(req.body.title);
   product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll();
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Em's Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
+  Product.fetchAll((products) => {
+    res.render("shop", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true,
+    });
   });
 };
