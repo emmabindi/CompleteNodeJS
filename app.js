4daeffd6764e5be449af0d2f0150bb2a2e73d926
Emma Bindi <emma.bindi@outlook.com>
2020-08-24 11:05:06 +1000

    Separated routes into controllers

--- app.js
+++ app.js
@@ -1,32 +1,22 @@
-// const http = require("http");
 const express = require("express");
 const path = require("path");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const bodyParser = require("body-parser");
+const errorController = require("./controllers/error")
 const app = express();
 
-// global config value
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-// this pkg calls next inside itself. It parses data sent thru forms
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// grants access for read only access to items in this folder:
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-  res.render("404", { pageTitle: "Page Not Found" });
-});
+app.use(errorController.get404);
 
-// const server = http.createServer(app);
-// server.listen(4000);
-// In Express the above 2 lines become:
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,28 +1,12 @@
+const path = require('path');
 const express = require("express");
+const productsController = require("../controllers/products");
 const router = express.Router();
-const path = require("path");
 
-const rootDir = require("../util/path");
-const products = [];
+// /admin/add-prodct => GET
+router.get("/add-product", productsController.getAddProduct);
 
-router.get("/add-product", (req, res, next) => {
-  // res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
-  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+// /admin/add-prodct => GET
+router.post("/add-product", productsController.postAddProduct);
 
-router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
-  products.push({ title: req.body.title });
-  res.redirect("/");
-});
-
-exports.routes = router;
-exports.products = products;
-// module.exports = router;
+module.exports = router;
